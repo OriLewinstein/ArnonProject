36aa3449bf394c8d031096fc1d2e6448d2ff272d
OriLewinstein <ori.lewinstein@gmail.com>
2022-04-23 19:58:02 +0300

    new link

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.querySelector(".button").addEventListener("click", function () {
     `https://finviz.com/quote.ashx?t=${ourValue}&ty=c&ta=1&p=d`
   );
   window3 = window.open(
-    `https://bigcharts.marketwatch.com/advchart/frames/frames.asp?show=&insttype=Stock&symb=${ourValue}&time=8&startdate=1%2F4%2F1999&enddate=4%2F22%2F2022&freq=1&compidx=aaaaa%3A0&comptemptext=&comp=none&ma=0&maval=9&uf=0&lf=67108864&lf2=0&lf3=0&type=64&style=320&size=4&x=50&y=18&timeFrameToggle=false&compareToToggle=false&indicatorsToggle=false&chartStyleToggle=false&state=15`
+    `https://bigcharts.marketwatch.com/advchart/frames/frames.asp?show=&insttype=Stock&symb=${ourValue}&time=12&startdate=1%2F4%2F1999&enddate=4%2F22%2F2022&freq=1&compidx=aaaaa%3A0&comptemptext=&comp=none&ma=0&maval=9&uf=0&lf=67108864&lf2=0&lf3=0&type=64&style=320&size=4&x=48&y=2&timeFrameToggle=false&compareToToggle=false&indicatorsToggle=false&chartStyleToggle=false&state=15`
   );
 });
 
