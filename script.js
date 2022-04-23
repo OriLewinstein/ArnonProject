a699efb22c766e8fc20e4cd26130aed7a8213da5
Nadav <eueu309@gmail.com>
2022-04-23 15:37:27 +0300

    d

--- script.js
+++ script.js
@@ -23,8 +23,7 @@ document.querySelector(".closebtn").addEventListener("click", function () {
   window2.close();
   window3.close();
 
-  document.querySelector(".search").value = " ";
-  document.location.reload;
+  document.location.reload();
 });
 
 const btn = document.querySelector(".button");
